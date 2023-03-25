86e1e8e0c66b533e446475f1f0bb5f98aa58cdbc
xpf1991 <xpftsh@126.com>
2023-03-25 10:41:19 +0800

    phonebook-part2-c-exercises is done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import FilterPart from './components/FilterPart'
 import FormAdd from './components/FormAdd'
 import NumberDisplay from './components/NumberDisplay'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState(
-    [
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]
-  )
-
+  const [persons, setPersons] = useState([])
   const [nameFilter, setNameFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect is on going')
+    axios
+      .get('http://localhost:3001/Persons')
+      .then(response => {
+        console.log('axios is on going')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log(`gets ${persons.length} person`)
 
   const handleNameFilter = (event) => {
     setNameFilter(event.target.value)
