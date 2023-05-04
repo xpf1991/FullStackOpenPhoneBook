d3b132b369468530ba71c3462e126df06cbb1f15
xpf1991 <xpftsh@126.com>
2023-05-04 17:26:52 +0800

    revise url

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3001/Persons'
+const url = '/api/persons'
 
 const getAll = () => {
     return axios.get(url).then(response => response.data)
